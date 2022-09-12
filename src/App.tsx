52b16f76f5175bb53564663c9a659b54bc68da1e
Samuel Hegner <samuel.hegner@gmail.com>
2022-09-13 00:04:07 +0100

    amazing site improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import samHead from './assets/samHead.png';
+import recordGif from './assets/RecordGif.gif';
 import './App.css';
 
 function App() {
@@ -10,16 +10,19 @@ function App() {
 		<div className='App'>
 			<div>
 				<h1>Samuel's Website</h1>
-				<h3>More to come...</h3>
+				<h3>More to come....</h3>
 			</div>
 			<div>
-				<button className='.logo' onClick={() => setCount((count) => count + 1)}>
-					<img src={samHead} />
+				<button onClick={() => setCount((count) => count + 1)}>
+					<img className='headClass' src={samHead} />
 				</button>
 			</div>
 			<h3>↓↓↓↓ I'm Samuel Hegner and I approve the message below ↓↓↓↓</h3>
 			<div className='boxedDiv'>
 				<h3>count is {count}</h3>
+			</div>
+			<div>
+				<img src={recordGif}></img>
 			</div>
 		</div>
 	);
